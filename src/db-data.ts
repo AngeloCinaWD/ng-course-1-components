86f9080ec3cef9899bf64467aff5adfbc325a208
unknown <a.cina@procreatech.com>
2024-09-13 11:36:38 +0200

    19. The Angular 17 @if else syntax

--- src/db-data.ts
+++ src/db-data.ts
@@ -12,8 +12,10 @@ export const COURSES: any = [
   {
     id: 2,
     description: "RxJs In Practice Course",
-    iconUrl:
-      "https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png",
+    // iconUrl:
+    //   "https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png",
+
+    iconUrl: "",
     longDescription:
       "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
     category: "BEGINNER",
