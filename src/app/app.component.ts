93d59931ac7f1a2d47f78b72c6a93ed661cbb0ad
unknown <a.cina@procreatech.com>
2024-09-17 08:55:50 +0200

    Interrompi 31. View Child - Learn the multiple View Query Configuration Options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { COURSES } from "../db-data";
 import { Course } from "./model/course";
 import { CourseCardComponent } from "./course-card/course-card.component";
@@ -36,12 +36,37 @@ export class AppComponent {
   @ViewChild(CourseCardComponent)
   card: CourseCardComponent;
 
+  // interrogo un elemento nel template tramite nome dato con TEMPLATE REFERENCE, lo richiamo inserendo il nome come stringa
+  // il collegamento tramite template reference è utile quando si hanno multipli elementi e se ne vuole interrogare uno in particolare
+  @ViewChild("cardRef")
+  card2: CourseCardComponent;
+
+  // il nome della variabile può essere uno qualsiasi, quello che conta è quello passato al ViewChild decorator
+  // con la template reference ed il tipo ElementRef ottengo l'html
+  @ViewChild("container")
+  container: ElementRef;
+
+  // posso ottenere un collegamento all'html di un component e non alla sua istanza indicandolo in un oggetto nel decorator
+  // la proprietà read è di default istance
+  @ViewChild("cardRef", { read: ElementRef })
+  collegamentoHTML: ElementRef;
+
+  // per interrogare elementi nativi del DOM, pieno html, utilizzo il tipo ElementRef
+
   onCardClicked(course: Course) {
     console.log(course);
-    // facendi il console log di card ottengo un riferimento ad una istanza del componente
+    // facendo il console log di card ottengo un riferimento ad una istanza del componente
     // le proprietà sono course, indice e count che ora non c'è perchè non è definita per questo compoennte
     // posso vedere tutte le sue prorietà e l'evento emesso (in questo caso courseSelected)
     console.log(this.card);
+    console.log(this.card2);
+    // l'oggetto container restituisce al suo interno un ElementRef con una prorpietà nativeElement che contiene una serie di proprietà legate all'html dell'elemento
+    console.log(this.container.nativeElement);
+    console.log(this.container.nativeElement.baseURI);
+    console.log(this.container.nativeElement.children);
+
+    // ottengo sempre un nativeElement
+    console.log(this.collegamentoHTML);
   }
 
   // funzione di tracciamento
